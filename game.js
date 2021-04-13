38c5059852f3f2c065ad8cfe79d2f57b60080b69
chittiliveclass <82372775+chittiliveclass@users.noreply.github.com>
2021-04-13 09:59:24 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ function checkAnswer(currentLevel) {
     } else {
       playSound("wrong");
       $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#level-title").text("Game Over, Reload the page to Restart");
 
       setTimeout(function () {
         $("body").removeClass("game-over");
